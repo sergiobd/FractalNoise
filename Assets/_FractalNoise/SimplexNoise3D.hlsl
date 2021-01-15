43057b1e51ca5379270e3c05143523cc02eee60b
Sergio Bromberg <sergioman@yahoo.com>
2021-01-15 17:11:57 +0900

    initial commit

---
+++ Assets/_FractalNoise/SimplexNoise3D.hlsl
@@ -0,0 +1,242 @@
+//
+// Simplex noise custom node for Shader Graph
+//
+// Original work (webgl-noise) Copyright (C) 2011 Ashima Arts.
+// Translation and modification was made by Keijiro Takahashi.
+// Ported to Shader Graph by Sergio L. Valladares and updated by André Mattos
+// Sergio Bromberg Added Fractal Noise Shader Graph node.
+// This shader is based on the webgl-noise GLSL shader. For further details
+// of the original shader, please see the following description from the
+// original source code.
+//
+//
+// Description : Array and textureless GLSL 2D/3D/4D simplex
+//               noise functions.
+//      Author : Ian McEwan, Ashima Arts.
+//  Maintainer : ijm
+//     Lastmod : 20110822 (ijm)
+//     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
+//               Distributed under the MIT License. See LICENSE file.
+//               https://github.com/ashima/webgl-noise
+//
+
+
+//UNITY_SHADER_NO_UPGRADE
+#ifndef SIMPLEXNOISE3D_INCLUDE
+#define SIMPLEXNOISE3D_INCLUDE
+
+
+float3 mod_3(float3 x, float3 y)
+{
+  return x - y * floor(x / y);
+}
+
+float3 mod289_3(float3 x)
+{
+    return x - floor(x / 289.0) * 289.0;
+}
+
+float4 mod289_4(float4 x)
+{
+    return x - floor(x / 289.0) * 289.0;
+}
+
+float4 permute(float4 x)
+{
+    return mod289_4((x * 34.0 + 1.0) * x);
+}
+
+float4 taylorInvSqrt(float4 r)
+{
+    return 1.79284291400159 - r * 0.85373472095314;
+}
+
+float3 fade(float3 t) {
+	return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
+}
+
+float4 snoise_grad(float3 v)
+{
+    const float2 C = float2(1.0 / 6.0, 1.0 / 3.0);
+
+    // First corner
+    float3 i  = floor(v + dot(v, C.yyy));
+    float3 x0 = v   - i + dot(i, C.xxx);
+
+    // Other corners
+    float3 g = step(x0.yzx, x0.xyz);
+    float3 l = 1.0 - g;
+    float3 i1 = min(g.xyz, l.zxy);
+    float3 i2 = max(g.xyz, l.zxy);
+
+    // x1 = x0 - i1  + 1.0 * C.xxx;
+    // x2 = x0 - i2  + 2.0 * C.xxx;
+    // x3 = x0 - 1.0 + 3.0 * C.xxx;
+    float3 x1 = x0 - i1 + C.xxx;
+    float3 x2 = x0 - i2 + C.yyy;
+    float3 x3 = x0 - 0.5;
+
+    // Permutations
+    i = mod289_3(i); // Avoid truncation effects in permutation
+    float4 p =
+        permute(permute(permute(i.z + float4(0.0, i1.z, i2.z, 1.0))
+                            + i.y + float4(0.0, i1.y, i2.y, 1.0))
+                            + i.x + float4(0.0, i1.x, i2.x, 1.0));
+
+    // Gradients: 7x7 points over a square, mapped onto an octahedron.
+    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
+    float4 j = p - 49.0 * floor(p / 49.0);  // mod(p,7*7)
+
+    float4 x_ = floor(j / 7.0);
+    float4 y_ = floor(j - 7.0 * x_);  // mod(j,N)
+
+    float4 x = (x_ * 2.0 + 0.5) / 7.0 - 1.0;
+    float4 y = (y_ * 2.0 + 0.5) / 7.0 - 1.0;
+
+    float4 h = 1.0 - abs(x) - abs(y);
+
+    float4 b0 = float4(x.xy, y.xy);
+    float4 b1 = float4(x.zw, y.zw);
+
+    //float4 s0 = float4(lessThan(b0, 0.0)) * 2.0 - 1.0;
+    //float4 s1 = float4(lessThan(b1, 0.0)) * 2.0 - 1.0;
+    float4 s0 = floor(b0) * 2.0 + 1.0;
+    float4 s1 = floor(b1) * 2.0 + 1.0;
+    float4 sh = -step(h, 0.0);
+
+    float4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+    float4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+
+    float3 g0 = float3(a0.xy, h.x);
+    float3 g1 = float3(a0.zw, h.y);
+    float3 g2 = float3(a1.xy, h.z);
+    float3 g3 = float3(a1.zw, h.w);
+
+    // Normalise gradients
+    float4 norm = taylorInvSqrt(float4(dot(g0, g0), dot(g1, g1), dot(g2, g2), dot(g3, g3)));
+    g0 *= norm.x;
+    g1 *= norm.y;
+    g2 *= norm.z;
+    g3 *= norm.w;
+
+    // Compute noise and gradient at P
+    float4 m = max(0.6 - float4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
+    float4 m2 = m * m;
+    float4 m3 = m2 * m;
+    float4 m4 = m2 * m2;
+    float3 grad =
+        -6.0 * m3.x * x0 * dot(x0, g0) + m4.x * g0 +
+        -6.0 * m3.y * x1 * dot(x1, g1) + m4.y * g1 +
+        -6.0 * m3.z * x2 * dot(x2, g2) + m4.z * g2 +
+        -6.0 * m3.w * x3 * dot(x3, g3) + m4.w * g3;
+    float4 px = float4(dot(x0, g0), dot(x1, g1), dot(x2, g2), dot(x3, g3));
+    return 42.0 * float4(grad, dot(m4, px));
+}
+
+void SimplexNoise3D_float (float3 Vertex, out float Noise, out float3 Gradient)
+{
+    float4 noise_vector = snoise_grad(Vertex);
+    Noise = noise_vector.w;
+    Gradient = noise_vector.xyz;
+}
+
+float pnoise(float3 P, float3 rep)
+{
+  float3 Pi0 = mod_3(floor(P), rep); // Integer part, modulo period
+  float3 Pi1 = mod_3(Pi0 + (float3)1.0, rep); // Integer part + 1, mod period
+  Pi0 = mod289_3(Pi0);
+  Pi1 = mod289_3(Pi1);
+  float3 Pf0 = frac(P); // Fractional part for interpolation
+  float3 Pf1 = Pf0 - (float3)1.0; // Fractional part - 1.0
+  float4 ix = float4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  float4 iy = float4(Pi0.y, Pi0.y, Pi1.y, Pi1.y);
+  float4 iz0 = (float4)Pi0.z;
+  float4 iz1 = (float4)Pi1.z;
+
+  float4 ixy = permute(permute(ix) + iy);
+  float4 ixy0 = permute(ixy + iz0);
+  float4 ixy1 = permute(ixy + iz1);
+
+  float4 gx0 = ixy0 / 7.0;
+  float4 gy0 = frac(floor(gx0) / 7.0) - 0.5;
+  gx0 = frac(gx0);
+  float4 gz0 = (float4)0.5 - abs(gx0) - abs(gy0);
+  float4 sz0 = step(gz0, (float4)0.0);
+  gx0 -= sz0 * (step((float4)0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step((float4)0.0, gy0) - 0.5);
+
+  float4 gx1 = ixy1 / 7.0;
+  float4 gy1 = frac(floor(gx1) / 7.0) - 0.5;
+  gx1 = frac(gx1);
+  float4 gz1 = (float4)0.5 - abs(gx1) - abs(gy1);
+  float4 sz1 = step(gz1, (float4)0.0);
+  gx1 -= sz1 * (step((float4)0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step((float4)0.0, gy1) - 0.5);
+
+  float3 g000 = float3(gx0.x,gy0.x,gz0.x);
+  float3 g100 = float3(gx0.y,gy0.y,gz0.y);
+  float3 g010 = float3(gx0.z,gy0.z,gz0.z);
+  float3 g110 = float3(gx0.w,gy0.w,gz0.w);
+  float3 g001 = float3(gx1.x,gy1.x,gz1.x);
+  float3 g101 = float3(gx1.y,gy1.y,gz1.y);
+  float3 g011 = float3(gx1.z,gy1.z,gz1.z);
+  float3 g111 = float3(gx1.w,gy1.w,gz1.w);
+
+  float4 norm0 = taylorInvSqrt(float4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  float4 norm1 = taylorInvSqrt(float4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, float3(Pf1.x, Pf0.y, Pf0.z));
+  float n010 = dot(g010, float3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, float3(Pf1.x, Pf1.y, Pf0.z));
+  float n001 = dot(g001, float3(Pf0.x, Pf0.y, Pf1.z));
+  float n101 = dot(g101, float3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, float3(Pf0.x, Pf1.y, Pf1.z));
+  float n111 = dot(g111, Pf1);
+
+  float3 fade_xyz = fade(Pf0);
+  float4 n_z = lerp(float4(n000, n100, n010, n110), float4(n001, n101, n011, n111), fade_xyz.z);
+  float2 n_yz = lerp(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = lerp(n_yz.x, n_yz.y, fade_xyz.x);
+  return 2.2 * n_xyz;
+}
+
+void FractalNoise_float(float3 Coordinates, int Octaves, float FractalAmplitudeFactor, float FractalFrequencyFactor, out float Noise) 
+{
+	const float epsilon = 0.0001;
+
+	float s = 1.0;
+	float w = 0.25;
+	float o = 0.5f;
+
+	for (int i = 0; i < Octaves; i++) {
+
+		float3 coord = Coordinates * s;
+		float3 period = float3(s, s, s) * 2.0;
+		
+		float vx, vy, vz;
+		float3 gradient;
+		
+		SimplexNoise3D_float(coord + float3(epsilon, 0, 0), vx, gradient);
+		SimplexNoise3D_float(coord + float3(0, epsilon, 0), vy, gradient);
+		SimplexNoise3D_float(coord + float3(0, 0, epsilon), vz, gradient);
+
+		o += w * float3(vx, vy , vz);
+		
+		s *= FractalFrequencyFactor;
+		w *= FractalAmplitudeFactor;
+
+	}
+
+	Noise = o;
+}
+
+#endif //SIMPLEXNOISE3D_INCLUDE
